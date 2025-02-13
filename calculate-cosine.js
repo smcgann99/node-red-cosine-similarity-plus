b154c715499e7c775c9ad5f750f83d2caace9f1b
smcgann99 <52132596+smcgann99@users.noreply.github.com>
2025-02-13 04:31:02 +0000

    updates

--- calculate-cosine.js
+++ calculate-cosine.js
@@ -2,34 +2,37 @@ const ERROR_VECTOR_LENGTH_ZERO = -100;
 const ERROR_COSINE_SIMILARITY_NAN = -200;
 const ERROR_FILE_PARSING = -500;
 const ERROR_VARIABLE_NOT_FOUND = -300;
-const ERROR_VARIABLE_NOT_ARRAY = -400;
+const ERROR_VARIABLE_NOT_OBJECT = -400;
 
 module.exports = function (RED) {
   function CalculateCosine(config) {
     RED.nodes.createNode(this, config);
+    const defaultThreshold = config.threshold || 0;
 
     function calculateSimilarityForVectors(inputVectors, storedVectors) {
-      const results = {};
+      const results = inputVectors.map(inputVector => {
+        const personResults = {};
 
-      for (const personName in storedVectors) {
-        if (storedVectors.hasOwnProperty(personName)) {
-          results[personName] = {};
+        for (const personName in storedVectors) {
+          if (storedVectors.hasOwnProperty(personName)) {
+            personResults[personName] = {};
 
-          for (const fileName in storedVectors[personName]) {
-            if (storedVectors[personName].hasOwnProperty(fileName)) {
-              const storedVector = storedVectors[personName][fileName];
-
-              results[personName][fileName] = inputVectors.map(inputVector => {
+            for (const fileName in storedVectors[personName]) {
+              if (storedVectors[personName].hasOwnProperty(fileName)) {
+                const storedVector = storedVectors[personName][fileName];
                 const result = cosineSimilarity(inputVector, storedVector);
                 if (result === ERROR_VECTOR_LENGTH_ZERO || result === ERROR_COSINE_SIMILARITY_NAN) {
-                  return result; // Return error code directly for that pair
+                  personResults[personName][fileName] = result; // Return error code directly for that pair
+                } else {
+                  personResults[personName][fileName] = result;
                 }
-                return result;
-              })[0]; // Directly assign the first (and only) value
+              }
             }
           }
         }
-      }
+
+        return personResults;
+      });
 
       return results;
     }
@@ -65,21 +68,38 @@ module.exports = function (RED) {
       if (fileType === "path") {
         try {
           const data = await fs.promises.readFile(filePath, "utf8");
-          return JSON.parse(data);
+          let validate= JSON.parse(data);
+          if (!isValidStoredVector(validate)) return ERROR_FILE_PARSING;
+          return validate;
         } catch (err) {
           return ERROR_FILE_PARSING;
         }
       } else if (fileType === "flow") {
         const storedVector = this.context().flow.get(filePath);
         if (!storedVector) return ERROR_VARIABLE_NOT_FOUND;
-        if (typeof storedVector !== 'object' || Array.isArray(storedVector)) return ERROR_VARIABLE_NOT_ARRAY;
+        if (!isValidStoredVector(storedVector)) return ERROR_FILE_PARSING;
         return storedVector;
       } else if (fileType === "global") {
         const storedVector = this.context().global.get(filePath);
         if (!storedVector) return ERROR_VARIABLE_NOT_FOUND;
-        if (typeof storedVector !== 'object' || Array.isArray(storedVector)) return ERROR_VARIABLE_NOT_ARRAY;
+        if (!isValidStoredVector(storedVector)) return ERROR_FILE_PARSING;
         return storedVector;
       }
+    }
+
+    function isValidStoredVector(storedVector) {
+      if (typeof storedVector !== 'object' || Array.isArray(storedVector)) return false;
+      for (const personName in storedVector) {
+        if (storedVector.hasOwnProperty(personName)) {
+          if (typeof storedVector[personName] !== 'object' || Array.isArray(storedVector[personName])) return false;
+          for (const fileName in storedVector[personName]) {
+            if (storedVector[personName].hasOwnProperty(fileName)) {
+              if (!Array.isArray(storedVector[personName][fileName])) return false;
+            }
+          }
+        }
+      }
+      return true;
     }
 
     this.on("input", async function (msg) {
@@ -89,21 +109,44 @@ module.exports = function (RED) {
       if (!Array.isArray(inputVectors) || inputVectors.length === 0) {
         this.error("Input vectors are not valid.");
       } else if (storedVectors === ERROR_FILE_PARSING) {
-        this.error("Stored vectors are not valid. Error occurred while parsing file.");
+        this.error("Stored vectors are not valid. Error occurred while parsing.");
       } else if (storedVectors === ERROR_VARIABLE_NOT_FOUND) {
         this.error("Stored vectors variable not found.");
-      } else if (storedVectors === ERROR_VARIABLE_NOT_ARRAY) {
-        this.error("Stored vectors variable is not an array.");
       } else {
-        let results = calculateSimilarityForVectors(inputVectors, storedVectors);
+        const threshold = msg.cosineOptions && !isNaN(msg.cosineOptions.threshold) && msg.cosineOptions.threshold >= 0 && msg.cosineOptions.threshold <= 1 && msg.cosineOptions.threshold !== ""
+          ? msg.cosineOptions.threshold
+          : defaultThreshold;
+
+        let results = calculateSimilarityForVectors(inputVectors, storedVectors, threshold);
         // Check for any error codes in the results
-        if (Object.values(results).some(person => Object.values(person).some(value => value === ERROR_VECTOR_LENGTH_ZERO))) {
+        if (results.some(personResults => Object.values(personResults).some(person => Object.values(person).some(value => value === ERROR_VECTOR_LENGTH_ZERO)))) {
           this.error("The vector length is 0, cannot calculate.", "Error");
-        } else if (Object.values(results).some(person => Object.values(person).some(value => value === ERROR_COSINE_SIMILARITY_NAN))) {
+        } else if (results.some(personResults => Object.values(personResults).some(person => Object.values(person).some(value => value === ERROR_COSINE_SIMILARITY_NAN)))) {
           this.error("The cosine similarity is NaN.", "Error");
         } else {
-          msg.payload = results;
-          this.send(msg);
+          // Filter results based on threshold
+          results = results.map(personResults => {
+            const filteredResults = {};
+            for (const personName in personResults) {
+              filteredResults[personName] = {};
+              for (const fileName in personResults[personName]) {
+                if (personResults[personName][fileName] >= threshold) {
+                  filteredResults[personName][fileName] = personResults[personName][fileName];
+                }
+              }
+            }
+            return filteredResults;
+          });
+
+          // Check if there are any results that meet the threshold
+          const hasResults = results.some(personResults => Object.keys(personResults).length > 0 && Object.values(personResults).some(person => Object.keys(person).length > 0));
+          if (!hasResults) {
+            this.error("No results - Try a lower threshold");
+          } else {
+            msg.payload = results;
+            msg.cosineConfig = { threshold: Number(threshold) };
+            this.send(msg);
+          }
         }
       }
     });
